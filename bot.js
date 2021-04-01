62cb7bbe829314410ae4718735a5e5557a6e1d24
Timothée Lefebvre <292261@supinfo.com>
2021-04-01 14:22:28 +0200

    xp message

--- bot.js
+++ bot.js
@@ -27,13 +27,14 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 client.on('message', async message => {
+    if (!message.guild) return;
+    if (message.author.bot) return;
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
     const randomXp = Math.floor(Math.random() * 9) + 1; //Random amont of XP until the number you want + 1
     const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomXp);
-    if (!message.guild) return;
-    if (message.author.bot) return;
+    
 
 
 
